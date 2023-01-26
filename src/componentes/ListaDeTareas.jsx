3a33387c81d2c0d2d4b1e8d811149a78930630a4
Lucas Cepeda <lucas.cepeda2@gmail.com>
2023-01-25 21:50:44 -0300

    anadiendo funcionalidad

--- src/componentes/ListaDeTareas.jsx
+++ src/componentes/ListaDeTareas.jsx
@@ -1,20 +1,38 @@
 import React, { useState } from "react";
 import TareaFormulario from './TareaFormulario';
 import '../hojas-de-estilo/ListaDeTareas.css';
+import Tarea from './Tarea';
 
 
 function ListaDeTareas() {
 
   const [tareas, setTareas] = useState([]);
 
-    return(
-      <>
-        <TareaFormulario/>
-        <div className="tareas-lista-contenedor">
-          LISTA DE TAREAS
-        </div>
-      </>
-    );
+  const agregarTarea = tarea => {
+    console.log(tarea );
+    if (tarea.texto.trim()){
+      tarea.texto = tarea.texto.trim();
+      const tareasActualizadas = [tarea, ...tareas];
+      setTareas(tareasActualizadas);
+    }
+  }
+
+  return(
+    <>
+      <TareaFormulario onSubmit={agregarTarea} />
+      <div className="tareas-lista-contenedor">
+        {
+          tareas.map((tarea) =>
+            <Tarea
+              key={tarea.id}
+              id={tarea.id}
+              texto={tarea.texto}
+              completada={tarea.completada} />
+          )
+        }
+      </div>
+    </>
+  );
 }
 
 export default ListaDeTareas;
